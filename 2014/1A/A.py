6ddfdca7811f51393cdaf002756e7ba0fcce1e74
David Peixotto <dmpots@gmail.com>
2014-04-26 09:27:11 -0700

    Solved A small
    Too slow for A large input.

--- 2014/1A/A.py
+++ 2014/1A/A.py
@@ -6,36 +6,72 @@ def pp(d):
   pp = pprint.PrettyPrinter(indent=4)
   pp.pprint(d)
 
+def flipv(s):
+  if s == "0":
+    return "1"
+  return "0"
+
 def flip(col):
-  def f(s):
-    if s == "0":
-      return "1"
-    return "0"
-  return [f(i) for i in col]
+  return [flipv(i) for i in col]
 
+def numify(row):
+  return int("".join(map(str,row)),2)
 
+def normalize(sockets):
+  return sorted(map(numify, sockets))
 
-def solve(goal, start):
-  flips = 0
-  for (i, col) in enumerate(goal):
-    if sorted(col) == sorted(start[i]):
-      continue
-    if sorted(flip(col)) == sorted(start[i]):
-      flips += 1
-    else:
-      return -1
-  return flips
+def apply_flips(flips, sockets):
+  results = []
+  for s in sockets:
+    res = []
+    for (i,v) in enumerate(s):
+      if i in flips:
+        res.append(flipv(v))
+      else:
+        res.append(v)
+    results.append("".join(res))
+  return results
 
 class Case:
   def __init__(self, i, start, goal):
     self.i = i
-    self.start = [list(x) for x in zip(*start)]
-    self.goal = [list(x) for x in zip(*goal)]
+    self.start = start
+    self.goal = goal
+    self.sorted_goal = normalize(goal)
+    self.cached_flips = set()
+
+  def is_sol(self,config):
+    return normalize(config) == self.sorted_goal
+
+  def findbest(self):
+    start = self.start
+    if self.is_sol(start):
+      return 0
+
+    num_flips = []
+    for g_row in self.goal:
+      for s_row in self.start:
+        flips = []
+        for (i,s) in enumerate(s_row):
+          if g_row[i] != s:
+            flips.append(i)
+        #print("{}: f:{} s:{} fd:{}".format(self.i,flips, start,flipped))
+        flips = tuple(flips)
+        if len(flips) == 0 or flips in self.cached_flips:
+          continue
+        flipped = apply_flips(flips, start)
+        if self.is_sol(apply_flips(flips, start)):
+          num_flips.append(len(flips))
+        self.cached_flips.add(flips)
+
+    if len(num_flips) == 0:
+      return -1
+    return min(num_flips)
 
   def solve(self):
-    s = solve(self.goal, self.start)
+    s = self.findbest()
     if s == -1:
-      sol = "NOT POSSIBLE"
+      sol="NOT POSSIBLE"
     else:
       sol = str(s)
     return "Case #{}: {}".format(self.i, sol)
@@ -55,6 +91,7 @@ def parse_input(stream):
 
 def main():
   cases = parse_input(sys.stdin)
+  #pp(cases)
   for case in cases:
     print(case.solve())
 
