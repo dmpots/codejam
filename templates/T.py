466c890f14591f956d438548930263d764ac7858
David Peixotto <dmpots@gmail.com>
2014-04-26 10:16:55 -0700

    Added python template file

---
+++ templates/T.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import sys
+import pprint
+import argparse
+
+debug = False
+def log(msg, *args):
+  global debug
+  if debug:
+    print(msg.format(*args))
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Google CodeJam Template')
+  parser.add_argument('-d', '--debug', action='store_true',
+                    help='print debugging messages')
+  parser.add_argument('-c', '--case', metavar='N', type=int,
+                    help='only run case N (default: run them all)')
+
+  args = parser.parse_args()
+  if args.debug:
+    global debug
+    debug = True
+  return args
+
+class Case:
+  def __init__(self, i):
+    self.i = i
+
+  def solve(self):
+    log("Solving: {}", self)
+    sol = ""
+    return self.case() + sol
+
+  def case(self):
+    return "Case #{}: ".format(self.i)
+
+  def __repr__(self):
+    return self.case()
+
+def parse_input(stream):
+  cases = []
+  N = int(stream.readline())
+  for i in range(1, N+1):
+    # parse case input
+    cases.append(Case(i))
+  return cases
+
+def main():
+  options = parse_args()
+  cases = parse_input(sys.stdin)
+  for case in cases:
+    if not options.case or options.case == case.i:
+      print(case.solve())
+
+
+main()
+  
